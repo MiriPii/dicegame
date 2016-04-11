e66d8eeceb5a1c6b7490bdad4e50b3407d22034f
Miri <miri.piiroinen@gmail.com>
2016-04-11 16:03:36 +0300

    Separated from state_manager.py

---
+++ dicegame/state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Miri Piiroinen
+# DiceGame
+
+class State(object):
+	"""The SuperClass for States.
+
+		:func update = Step forward in time and update modified parameters
+		:func draw = Draw the visuals into the specified window."""
+
+	def __init__(self, machine):
+
+		self.machine = machine
+		# self.window = window
+		self.nextSt = None
+
+	def update(self):
+		pass
+
+	def draw(self):
+		pass
+
+	def getNext(self):
+		return self.nextSt
---
+++ dicegame/state_machine.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Miri Piiroinen
+# DiceGame
+
+
+class StateMachine:
+
+	def __init__(self):
+		self.running = False
+		self.states = []
+
+	def load(self, state):
+
+		self.running = True
+		self.states.append(state)
+
+	def isRunning(self):
+		return self.running
+
+	def quit(self):
+		self.running = False
+
+	def nextState(self):
+
+		if len(self.states) != 0:
+			temp = self.states[len(self.states)-1].getNext()
+
+			if (temp != None):
+
+				self.states.append(temp)
+
+	def update(self):
+		# Allows the state to update the game
+		self.states[len(self.states)-1].update()
+
+	def draw(self):
+		#Allows the state to draw to the active window
+		self.states[len(self.states)-1].draw()
