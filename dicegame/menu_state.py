a155ff02358c8617d71fbfc01ab22fe16fcc2892
Miri <miri.piiroinen@gmail.com>
2016-04-14 11:53:50 +0300

    Modded menu_state from intro_state

--- dicegame/menu_state.py
+++ dicegame/menu_state.py
@@ -31,32 +31,80 @@ class MenuState(State):
         # Set BackGround
         self.bg = pygame.Surface(self.display.get_size())
         self.bg = self.bg.convert()
-        self.bg.fill(GREEN)
+        self.bg.fill(BLUE)
 
         # Screen size
         scr_w = self.bg.get_width()
         scr_h = self.bg.get_height()
 
+        # Menu traverse helper
+        self.hl_pos = 0
+
         # Set fonts and menu items
         font_path = "./../fonts/"
         self.text_font = pygame.font.Font(None, 24)
+        self.title_font = pygame.font.Font(font_path+"justice.ttf", 80)
+        self.title_font.set_underline(True)
+        self.font = pygame.font.Font(font_path+"justice.ttf", 42)
+        self.font_hl = pygame.font.Font(font_path+"justice3d.ttf", 42)
+
+        # Set Title
+        self.title = self.title_font.render("DiceGame", True, BLACK)
+        self.titlepos = self.title.get_rect()
+        self.titlepos.centerx = self.bg.get_rect().centerx
+        self.bg.blit(self.title, self.titlepos)
 
         # Set instructions
-        self.info = self.text_font.render("This is test screen. ESC to QUIT.",
+        self.info = self.text_font.render("Use UP and DOWN Arrows to select.",
                                         0, BLACK)
         self.inforect = self.info.get_rect()
         self.inforect.centerx = self.bg.get_rect().centerx
         self.inforect.y = scr_h-80
 
-        self.info2 = self.text_font.render("Press SPACE-key to load next state.", 0, BLACK)
+        self.info2 = self.text_font.render("Press Return-key to confirm your selection.", 0, BLACK)
         self.info2rect = self.info2.get_rect()
         self.info2rect.centerx = self.bg.get_rect().centerx
         self.info2rect.y = scr_h-50
         self.bg.blit(self.info, self.inforect)
         self.bg.blit(self.info2, self.info2rect)
 
+        # Set items
+        self.start = self.font.render("Play Again", 0, BLACK)
+        self.start_hl = self.font_hl.render("Play Again", 0, BLACK)
+        self.quit = self.font.render("QUIT", 0, BLACK)
+        self.quit_hl = self.font_hl.render("QUIT", 0, BLACK)
+        # Set locations for the menu items
+        self.startrect = self.start.get_rect()
+        self.quitrect = self.quit.get_rect()
+        # Center x
+        self.startrect.centerx = self.bg.get_rect().centerx
+        self.quitrect.centerx = self.bg.get_rect().centerx
+        # Set y
+        self.startrect.y = scr_h//2
+        self.quitrect.y = scr_h//2+80
+
+        """
+        # Cursor : WIP
+        self.cursor_l = self.font.render(">", True, BLACK)
+        self.cursor_r = self.font.render("<", True, BLACK)
+        """
+        """ Making cursor wrap around selections
+        # Wrap cursors around
+        self.cursor_l_rect = self.cursor_l.get_rect()
+        self.cursor_l_rect.x = self.
+        self.cursor_r_rect = self.cursor_r.get_rect()
+        """
+
         # Draw the initial display
         self.display.blit(self.bg, (0, 0))
+        self.display.blit(self.start_hl, self.startrect)
+        self.display.blit(self.quit, self.quitrect)
+
+    def move(self, direction):
+        if (direction == pygame.K_DOWN):
+            self.hl_pos = 1
+        elif (direction == pygame.K_UP):
+            self.hl_pos = 0
 
     def update(self):
 
@@ -73,41 +121,31 @@ class MenuState(State):
                 if event.key == pygame.K_ESCAPE:
                     self.machine.quit()
 
-                elif event.key == pygame.K_SPACE:
-                    self.nextSt = game_state.GameState(self.machine, self.display)
-        '''
-        print("")
-        print("------------")
-        print("Would you like to play a new game or quit?")
-        print("Choose 'Y' to start a new game.")
-        print("Choose 'N' to quit the game.")
-        answer = input()
+                elif ((event.key == pygame.K_DOWN) or (event.key == pygame.K_UP)):
+                    self.move(event.key)
 
-        while answer.lower() not in {'y', 'n'}:
-            print("Only (Y) and (N) are accepted!")
-            answer = input()
+                elif event.key == pygame.K_RETURN:
 
-        if answer.lower() == 'y':
-            print("")
-            print("------------")
-            print("Great! I wish you the best luck!")
-            input("Press any key to start the game.")
-            self.nextSt = game_state.GameState(self.machine, self.display)
-        else:
-            print("")
-            print("------------")
-            print("You are leaving already?")
-            print("I hope to see you soon! Have a great day!")
-            print("And don't forget to smile!")
-            input("Press any key to close the game.")
-            self.machine.quit()
-        '''
+                    if self.hl_pos == 0:
+                        self.nextSt = game_state.GameState(self.machine, self.display)
+                    elif self.hl_pos == 1:
+                        self.machine.quit()
 
     def draw(self):
         if self.init is True:
             print(" MenuState draw called ")
             print(" MenuState working normally ")
             self.init = False
+
+        self.display.fill(BLUE)                               # Clear screen
+        self.display.blit(self.bg, (0, 0))                    # Draw background
+
+        if self.hl_pos == 1:                                # #
+            self.display.blit(self.start, self.startrect)     #
+            self.display.blit(self.quit_hl, self.quitrect)    # Draw menu elements
+        else:                                                 #
+            self.display.blit(self.start_hl, self.startrect)  #
+            self.display.blit(self.quit, self.quitrect)     # #
 
         # Draw updates onto display
         pygame.display.flip()
